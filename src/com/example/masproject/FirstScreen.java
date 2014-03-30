ef70e522f5b2500afcc62a8d018227e2b63ac8e7
Xcelsziore <aiswarianair90@gatech.edu>
2014-03-30 01:14:39 -0400

    Two working screens

---
+++ src/com/example/masproject/FirstScreen.java
@@ -0,0 +1,71 @@
+package com.example.masproject;
+
+import java.util.Locale;
+
+
+
+import com.example.masproject.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.masproject.HTTPInteraction;
+
+public class FirstScreen extends Activity {
+
+	 Button b1;
+	 Button b2;
+	 TextView inputName;
+	 TextView inputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
+        System.out.println("You are here.");
+        
+        inputName = (TextView) findViewById(R.id.name);
+ //       inputPassword = (TextView) findViewById(R.id.);
+        
+        b1 = (Button) findViewById(R.id.button1);
+       b2 = (Button) findViewById(R.id.button2);
+        
+        b1.setOnClickListener(myhandler1);
+        b2.setOnClickListener(myhandler2);
+
+    }
+
+    View.OnClickListener myhandler1 = new View.OnClickListener() {
+        public void onClick(View v) {
+        	
+        	
+        	//Starting a new Intent
+            Intent nextScreen = new Intent(getApplicationContext(), SecondScreen.class);
+
+            //Sending data to another Activity
+            nextScreen.putExtra("name", inputName.getText().toString());
+            //nextScreen.putExtra("email", inputPassword.getText().toString());
+
+          //  Log.e("n", inputName.getText()+"."+ inputPassword.getText());
+
+            startActivity(nextScreen);
+        }
+      };
+      View.OnClickListener myhandler2 = new View.OnClickListener() {
+        public void onClick(View v) {
+          // it was the 2nd button
+        }
+      };
+    
+
+    
+}
+
+
+
---
+++ src/com/example/masproject/HTTPInteraction.java
@@ -0,0 +1,82 @@
+package com.example.masproject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.os.StrictMode;
+import android.util.Log;
+
+public class HTTPInteraction {
+
+	InputStream is;
+
+	public HTTPInteraction() {
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+				.detectNetwork() // or .detectAll() for all detectable problems
+				.penaltyDialog() // show a dialog
+				.permitNetwork() // permit Network access
+				.build());
+		is = null;
+	}
+
+	public InputStream httpRequest(String url,
+			ArrayList<NameValuePair> nameValuePairs) {
+
+		// http post
+		try {
+
+			System.out.println("test1");
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(url);
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			System.out.println("test2");
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+			System.out.println(is + "response");
+
+		}
+
+		catch (Exception e) {
+			Log.e("log_tag", "Error in http connection " + e.toString());
+		}
+		return is;
+	}
+
+	public JSONArray parseHttpResponse() {
+
+		JSONArray jArray = null;
+		try {
+			String line = null;
+			String jsonString = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			jsonString = sb.toString();
+			jArray = new JSONArray(jsonString);
+		} catch (Exception e) {
+			Log.e("log_tag",
+					"Error in http connection and jsonh" + e.toString());
+		}
+		return jArray;
+	}
+
+}
+
